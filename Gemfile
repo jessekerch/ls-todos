9764baede763813a8149f4652af6264fe20eb7cd
Jesse Work <JesseMacAir@macbook-pro-4.lan>
2023-04-24 14:41:22 -0700

    Add To Do list functions to todo.rb and views

--- Gemfile
+++ Gemfile
@@ -1,8 +1,13 @@
 source "https://rubygems.org"
 
+gem "pry"
 gem "sinatra"
 gem "sinatra-contrib"
 gem "erubis"
 gem "webrick"
 
 ruby "3.2.2"
+
+group :production do
+  gem "puma"
+end
--- todo.rb
+++ todo.rb
@@ -1,5 +1,7 @@
+require "pry"
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
+require "sinatra/content_for"
 require "tilt/erubis"
 
 configure do
@@ -12,13 +14,44 @@ before do
 end
 
 helpers do
-  # Return error message if list name invalid, nil if valid
-  def error_for_list_name(name)
-    if !(1..100).cover? name.size
-      "List name must be between 1 and 100 characters."
-    elsif session[:lists].any? {|list| list[:name] == name}
-      "List name must be unique."
-    end
+  def list_completed?(list)
+    todos_count(list) > 0 && todos_remaining_count(list) == 0
+  end
+
+  def list_class(list)
+    "complete" if list_completed?(list)
+  end
+
+  def todo_class(todo)
+    "complete" if todo[:completed]
+  end
+
+  def todos_remaining_count(list)
+    list[:todos].count {|todo| !todo[:completed]}
+  end
+
+  def todos_count(list)
+    list[:todos].size
+  end
+
+  def sort_lists(lists, &block)
+    incomplete_lists = {}
+    complete_lists = {}
+
+    complete_lists, incomplete_lists = lists.partition { |list| list_completed?(list) }
+
+    incomplete_lists.each { |list| yield list, lists.index(list) }
+    complete_lists.each  { |list| yield list, lists.index(list) }
+  end
+
+  def sort_todos(todos, &block)
+    incomplete_todos = {}
+    complete_todos = {}
+
+    complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
+
+    incomplete_todos.each { |todo| yield todo, todos.index(todo) }
+    complete_todos.each  { |todo| yield todo, todos.index(todo) }
   end
 end
 
@@ -29,12 +62,29 @@ end
 # View list of lists
 get "/lists" do
   @lists = session[:lists]
+
   erb :lists, layout: :layout
 end
 
 # Render a new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
+end
+
+# Return error message if list name invalid, nil if valid
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "List name must be unique."
+  end
+end
+
+# Return error message if list name invalid, nil if valid
+def error_for_todo_name(name)
+  if !(1..100).cover? name.size
+    "Todo must be between 1 and 100 characters."
+  end
 end
 
 # Create a new list
@@ -49,4 +99,103 @@ post "/lists" do
     session[:success] = "The list has been created."
     redirect "/lists"
   end
+end
+
+# View to do items on a list
+get "/lists/:number" do
+  @list_num = params[:number].to_i
+  @list = session[:lists][@list_num]
+
+  erb :list_detail, layout: :layout
+end
+
+# Get page to edit a list name
+get "/lists/:number/edit" do
+  list_num = params[:number].to_i
+  @list = session[:lists][list_num]
+
+  erb :edit_list, layout: :layout
+end
+
+# Edit a list name
+post "/lists/:number" do
+  list_name = params[:list_name].strip
+  list_num = params[:number].to_i
+  @list = session[:lists][list_num]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list name has been updated."
+    redirect "/lists/#{list_num}"
+  end
+end
+
+# Delete a list
+post "/lists/:number/delete" do
+  list_num = params[:number].to_i
+  list = session[:lists][list_num][:name]
+  session[:lists].delete_at(list_num)
+
+  session[:success] = "#{list_name} has been deleted."
+  redirect "/lists"
+end
+
+# Add a new todo to a list
+post "/lists/:list_num/todos" do
+  @list_num = params[:list_num].to_i
+  @list = session[:lists][@list_num]
+  todo_name = params[:todo].strip
+
+  error = error_for_todo_name(todo_name)
+  if error
+    session[:error] = error
+    erb :list_detail, layout: :layout
+  else
+    @list[:todos] << {name: params[:todo], completed: false}
+    session[:success] = "The todo has been added."
+    redirect "/lists/#{@list_num}"
+  end
+end
+
+# Delete a todo
+post "/lists/:list_num/todos/:todo_num/delete" do
+  @list_num = params[:list_num].to_i
+  @list = session[:lists][@list_num]
+
+  todo_num = params[:todo_num].to_i
+  @list[:todos].delete_at(todo_num)
+
+  session[:success] = "Todo has been deleted."
+  redirect "/lists/#{@list_num}"
+end
+
+# Check or uncheck a todo checkbox
+post "/lists/:list_num/todos/:todo_num" do
+  @list_num = params[:list_num].to_i
+  @list = session[:lists][@list_num]
+
+  todo_num = params[:todo_num].to_i
+
+  checked = params[:completed] == "true"
+  @list[:todos][todo_num][:completed] = checked
+
+  session[:success] = "Todo item has been updated."
+  redirect "/lists/#{@list_num}"
+end
+
+# Check all todo checkboxes
+post "/lists/:list_num/completeall" do
+  @list_num = params[:list_num].to_i
+  @list = session[:lists][@list_num]
+
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+
+  session[:success] = "All todo items have been completed."
+  redirect "/lists/#{@list_num}"
 end
